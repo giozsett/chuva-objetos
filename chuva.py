ee114c83a9d89c1803ecda1fc66a8df1902d59b5
Giovana Z Corsetti <giovana.corsetti@outlook.com>
2023-09-30 12:44:37 -0300

    player voa e tem movimento

--- chuva.py
+++ chuva.py
@@ -7,7 +7,7 @@ pygame.init()
 tamanho = (960, 540)
 tela = pygame.display.set_mode(tamanho)
 
-# Importa arquivos de imagem necessários
+# Importa arquivos de imagem referentes ao plano de fundo
 plano_fundo = pygame.image.load('assets/fundo/Night-Background8.png').convert()
 pedras_flutuantes = pygame.image.load('assets/fundo/Night-Background1.png').convert_alpha()
 lua_esfera = pygame.image.load('assets/fundo/Night-Background2.png').convert_alpha()
@@ -17,11 +17,26 @@ estrela_amarela = pygame.image.load('assets/fundo/Night-Background5.png').conver
 estrela_laranja = pygame.image.load('assets/fundo/Night-Background6.png').convert_alpha()
 estrela_roxa = pygame.image.load('assets/fundo/Night-Background7.png').convert_alpha()
 
-# Importa o personagem
-jogador_parado_surf = pygame.image.load('assets/jogador/parado/Hero Boy Idle1.png').convert_alpha()
-jogador_parado_rect = jogador_parado_surf.get_rect( midbottom = (100, 530))
 
-# Transforma o tamanho da imagem de fundo
+# Carrega as imagens do personagem parado através de uma lista.
+# Cria um loop para guardar nessa lista.
+# O jogador index cria o retângulo de colisão.
+jogador_index = 0
+jogador_parado_superficie = []
+for imagem in range(1, 14):
+    img = pygame.image.load(f'assets/jogador/parado/Hero Boy Idle{imagem}.png').convert_alpha()
+    jogador_parado_superficie.append(img)
+jogador_retangulo = jogador_parado_superficie[jogador_index].get_rect(center = (100, 430))
+
+# Carrega imagens do jogador voando.
+# (Mesmo método do jogador parado.)
+jogador_voando_superficie = []
+for imagem in range(1, 9):
+    img = pygame.image.load(f'assets/jogador/voar/Hero Boy Fly{imagem}.png').convert_alpha()
+    jogador_voando_superficie.append(img)
+
+
+# Transforma o tamanho das imagens de fundo para o tamanho correto
 plano_fundo = pygame.transform.scale(plano_fundo, tamanho)
 pedras_flutuantes = pygame.transform.scale(pedras_flutuantes, tamanho)
 lua_esfera = pygame.transform.scale(lua_esfera, tamanho)
@@ -37,23 +52,46 @@ pygame.display.set_caption("ChuvaMortal")
 # Cria um relógio para o FPS
 relogio = pygame.time.Clock()
 
-# Loop principal do jogo
+# Controla se o personagem está se movendo 
+# (se o valor for positivo, direita e se for negativo, esqueda)
 movimento_personagem = 0
 
+# Loop principal do jogo
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-        if evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_RIGHT:
-                movimento_personagem = 5
-            
-            if evento.key == pygame.K_LEFT:
-                movimento_personagem = -5
+    # Calcula o movimento do jogador
+    jogador_retangulo.x += movimento_personagem
+    # jogador_superficie = null
+        
+    # Faz o protagonista aparecer na tela parado.
+    if movimento_personagem == 0:
+        jogador_superficies = jogador_parado_superficie
 
+    else:
+        jogador_superficies = jogador_voando_superficie
+        if jogador_index >= len(jogador_voando_superficie) - 1:
+            jogador_index = 0
+        
+        # Jogador parado quando solta a tecla.
+    if evento.type == pygame.KEYUP:
+            movimento_personagem = 0
+            tela.blit(jogador_parado_superficie[int(jogador_index)], jogador_retangulo)
+       
+       # Movimentação do personagem (direita e esquerda).
+    if evento.type == pygame.KEYDOWN:
+        if evento.key == pygame.K_RIGHT:
+            movimento_personagem = 5
 
+        if evento.key == pygame.K_LEFT:
+            movimento_personagem = -5
+                
+
+    # Faz os elementos aparecerem na tela.
+        # Os elementos de cima estão atrás.
     tela.blit(plano_fundo, (0,0))
     tela.blit(estrela_roxa, (0,0))
     tela.blit(estrela_laranja, (0,0))
@@ -63,16 +101,17 @@ while True:
     tela.blit(lua_esfera, (0,0))
     tela.blit(pedras_flutuantes, (0,0))
 
-    # Desenha o jogador na tela
-    jogador_parado_rect.x += movimento_personagem
-    tela.blit(jogador_parado_surf, jogador_parado_rect)
-
-
-    
-    
+    # Jogador aparecendo parado na tela.
+    # Movimentação dele parado de acordo com o número de frames que tem na lista.
+    # O 'len' significa lenght, que é o tamanho da lista.
+    tela.blit(jogador_superficies[int(jogador_index)], jogador_retangulo)
+    jogador_index += 0.12
+    if jogador_index >= len(jogador_parado_superficie) - 1:
+        jogador_index = 0
 
     # Atualiza a tela com o conteúdo.
     pygame.display.update()
 
-    #Define a quantidade de frames por segundo
+    #Define a quantidade de frames por segundo.
+    # 60 vezes por segundo.
     relogio.tick(60)
